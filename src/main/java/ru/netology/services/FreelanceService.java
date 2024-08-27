a280cdb5c213470e7090e4dcbfbcef5ef1bf6401
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-08-27 17:55:14 +0400

    fix the syntax

--- src/main/java/ru/netology/services/FreelanceService.java
+++ src/main/java/ru/netology/services/FreelanceService.java
@@ -2,21 +2,19 @@ package ru.netology.services;
 
 public class FreelanceService {
 
-    public int calculate(int arg_income, int arg_expenses, int arg_threshold) {
+    public int calculate(int income, int expenses, int threshold) {
         int count = 0;
         int money = 0;
-        int income = arg_income;
-        int expenses = arg_expenses;
-        int threshold = arg_threshold;
         for (int month = 1; month <= 12; month++) {
 
             if (money >= threshold) {
                 count++;
-                int tmp_money = money - expenses- (money - expenses)/3;
-                System.out.println("Месяц "+month+". Денег "+money+". Буду отдыхать. Потратил -"+expenses+", затем ещё -"+ tmp_money);
-                money = (money - expenses)/3;
+                int tmpMoney = money - expenses - (money - expenses) / 3;
+
+                System.out.println("Месяц " + month + ". Денег " + money + ". Буду отдыхать. Потратил -" + expenses + ", затем ещё -" + tmpMoney);
+                money = (money - expenses) / 3;
             } else {
-                System.out.println("Месяц "+month+". Денег "+money+". Придётся работать. Заработал +"+income+", потратил -"+expenses);
+                System.out.println("Месяц " + month + ". Денег " + money + ". Придётся работать. Заработал +" + income + ", потратил -" + expenses);
                 money += (income - expenses);
             }
         }
